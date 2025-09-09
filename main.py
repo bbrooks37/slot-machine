cfb480ae6cb9ea754bdbf272f57ad964601415f0
bbrooks37 <brandon37.brooks@gmail.com>
2025-09-09 15:12:00 -0400

    etting up functions for the lot machine

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposit():
     while True:
         amount = input("Enter amount to deposit: $")
@@ -12,4 +16,43 @@ def deposit():
     
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a number between 1 and 3.")
+        else:
+            print("Invalid input. Please enter a numeric value.")
+    
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Please enter a bet between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("Invalid input. Please enter a numeric value.")
+    return amount
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    bet = get_bet()
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    if total_bet > balance:
+        print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+    else:
+        print(f"You have placed a bet of ${total_bet}. You have ${balance - total_bet} remaining.")
+   
+
+main()
